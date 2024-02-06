36f622522b48ebf2f5edfc311922f379148ddd7a
Hyarius <Boissy.Jeremy@yahoo.fr>
2024-02-06 18:56:09 +0100

    Adding comment on Shader instructions

--- Sparkle/src/graphics/opengl/spk_opengl_shader_instruction.hpp
+++ Sparkle/src/graphics/opengl/spk_opengl_shader_instruction.hpp
@@ -6,6 +6,27 @@
 
 namespace spk::OpenGL
 {
+	/**
+	 * @brief Represents and categorizes a single instruction or declaration within OpenGL shader code, used internally for shader management.
+	 *
+	 * ShaderInstruction facilitates the parsing and categorization of GLSL shader code, enabling the internal system to interpret and manage
+	 * shader instructions more efficiently. Each instance of this class is associated with a specific type of shader instruction, such as
+	 * uniform blocks, samplers, or functions, and contains both the raw GLSL code and extracted metadata in a structured format.
+	 *
+	 * Although primarily intended for internal use within the shader parsing and generation system, this documentation provides insight into
+	 * how shader code is analyzed and processed. This can aid in understanding the capabilities and limitations of the system's shader management.
+	 *
+	 * Main Components:
+	 * - type: Identifies the instruction type, aiding in its processing and management.
+	 * - content: Stores the GLSL code snippet corresponding to the instruction.
+	 * - informations: A JSON object encapsulating detailed information extracted from the instruction, useful for further processing or analysis.
+	 *
+	 * The `parseShaderInstruction` static method is a key part of this class, analyzing GLSL code to identify and organize its constituent
+	 * instructions. This method is crucial for the dynamic handling of shaders, supporting operations such as modification, optimization,
+	 * or conditional inclusion of shader code.
+	 *
+	 * Note: This class and its methods are designed for internal framework use and are not intended to be directly utilized by end-users.
+	 */
 	struct ShaderInstruction
 	{
 		static inline const std::string ValueKey = "Value";
