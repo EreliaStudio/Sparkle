7c2d7315fc1bad77e36da14474a17990bd4a5c0b
Hyarius <Boissy.Jeremy@yahoo.fr>
2024-02-08 20:36:14 +0100

    Commenting JSON folder

--- Sparkle/src/miscellaneous/JSON/spk_JSON_file.hpp
+++ Sparkle/src/miscellaneous/JSON/spk_JSON_file.hpp
@@ -22,27 +22,27 @@ namespace spk
 		 * @code
 		 * // Create a File instance
 		 * spk::JSON::File jsonFile;
-		 * 
+		 *
 		 * // Load JSON data from a file
 		 * jsonFile.load("example.json");
-		 * 
+		 *
 		 * // Check if a key exists in the JSON data
 		 * if (jsonFile.contains("intValue"))
 		 * {
 		 *     // Access an integer value from the JSON data using operator[]
 		 *     const spk::JSON::Object& intValueObject = jsonFile["intValue"];
-		 *     
+		 *
 		 *     // Use the as method to convert the value to an integer
 		 *     int intValue = intValueObject.as<int>();
 		 *     std::cout << "Integer Value: " << intValue << std::endl;
 		 * }
-		 * 
+		 *
 		 * // Check if another key exists in the JSON data
 		 * if (jsonFile.contains("stringValue"))
 		 * {
 		 *     // Access a string value from the JSON data using operator[]
 		 *     const spk::JSON::Object& stringValueObject = jsonFile["stringValue"];
-		 *     
+		 *
 		 *     // Use the as method to convert the value to a string
 		 *     std::string stringValue = stringValueObject.as<std::string>();
 		 *     std::cout << "String Value: " << stringValue << std::endl;
@@ -54,27 +54,106 @@ namespace spk
 		private:
 			spk::JSON::Object _root;
 
-		public:
+		public: /**
+				 * @brief Default constructor for the JSON File class.
+				 *
+				 * Initializes a new instance of the File class without specifying a file path. The JSON object
+				 * contained within this instance is initially empty.
+				 */
 			File();
-			File(const std::filesystem::path& p_filePath);
 
-			void load(const std::filesystem::path& p_filePath);
-			void save(const std::filesystem::path& p_filePath) const;
+			/**
+			 * @brief Constructor for the JSON File class with a file path.
+			 *
+			 * Initializes a new instance of the File class and automatically loads JSON data from the specified
+			 * file path.
+			 *
+			 * @param p_filePath The path of the file from which to load JSON data.
+			 */
+			File(const std::filesystem::path &p_filePath);
 
-			bool contains(const std::string& p_key) const;
+			/**
+			 * @brief Loads JSON data from a file.
+			 *
+			 * Reads JSON data from the specified file path and populates the internal JSON object with this data.
+			 *
+			 * @param p_filePath The path of the file from which to load JSON data.
+			 */
+			void load(const std::filesystem::path &p_filePath);
 
+			/**
+			 * @brief Saves the JSON data to a file.
+			 *
+			 * Serializes the internal JSON object and writes it to the specified file path.
+			 *
+			 * @param p_filePath The path of the file to which the JSON data will be saved.
+			 */
+			void save(const std::filesystem::path &p_filePath) const;
+
+			/**
+			 * @brief Checks if the JSON data contains a specific key.
+			 *
+			 * Determines whether the internal JSON object contains an attribute with the specified key.
+			 *
+			 * @param p_key The key to search for in the JSON data.
+			 * @return True if the key exists, false otherwise.
+			 */
+			bool contains(const std::string &p_key) const;
+
+			/**
+			 * @brief Accesses a JSON object by key.
+			 *
+			 * Retrieves a reference to a JSON object associated with the specified key.
+			 *
+			 * @param p_key The key of the JSON object to retrieve.
+			 * @return A const reference to the JSON object associated with the specified key.
+			 */
 			const spk::JSON::Object &operator[](const std::string &p_key) const;
 
+			/**
+			 * @brief Accesses a JSON object by index.
+			 *
+			 * Retrieves a reference to a JSON object at the specified index within an array.
+			 *
+			 * @param p_index The index of the JSON object within an array.
+			 * @return A const reference to the JSON object at the specified index.
+			 */
 			const spk::JSON::Object &operator[](size_t p_index) const;
 
+			/**
+			 * @brief Retrieves the stored value as the specified type.
+			 *
+			 * Accesses the value stored in the root JSON object as the specified type. Throws an exception if the actual type of the stored
+			 * value does not match the requested type.
+			 *
+			 * @tparam TType The type to cast the stored value to.
+			 * @return A const reference to the stored value cast to the specified type.
+			 */
 			template <typename TType>
 			const TType &as()
 			{
 				return (_root.as<TType>());
 			}
 
+			/**
+			 * @brief Retrieves the root JSON object.
+			 *
+			 * Provides access to the root JSON object of the file, allowing for direct manipulation and query of the JSON structure.
+			 *
+			 * @return A const reference to the root JSON object.
+			 */
 			const spk::JSON::Object &root() const;
 
+			/**
+			 * @brief Outputs the JSON file to a standard output stream.
+			 *
+			 * Serializes the JSON file to a string and writes it to the provided output stream. Supports pretty printing based on
+			 * indentation settings of the JSON object.
+			 *
+			 * @param p_os The output stream to write the serialized JSON file to.
+			 * @param p_file The JSON file to serialize and write.
+			 * @return A reference to the updated output stream.
+			 */
 			friend std::ostream &operator<<(std::ostream &p_os, const File &p_file);
 		};
 	}
--- Sparkle/src/miscellaneous/JSON/spk_JSON_object.hpp
+++ Sparkle/src/miscellaneous/JSON/spk_JSON_object.hpp
@@ -40,8 +40,8 @@ namespace spk
 		class Object
 		{
 		public:
-			using Unit = std::variant<bool, long, double, std::string, Object*, std::nullptr_t>; /**< The type for a single JSON value. */
-			using ContentType = std::variant<Unit, std::map<std::string, Object*>, std::vector<Object*>>; /**< The type for the content of the JSON object. */
+			using Unit = std::variant<bool, long, double, std::string, Object *, std::nullptr_t>;			/**< The type for a single JSON value. */
+			using ContentType = std::variant<Unit, std::map<std::string, Object *>, std::vector<Object *>>; /**< The type for the content of the JSON object. */
 
 		private:
 			bool _initialized;
@@ -50,33 +50,205 @@ namespace spk
 			static size_t _indent;
 			const static uint8_t _indentSize = 4;
 
-		public:
-			Object(const std::string& p_name = "Unnamed");
+			void printUnit(std::ostream &p_os) const;
+			void printObject(std::ostream &p_os) const;
+			void printArray(std::ostream &p_os) const;
 
+		public:
+			/**
+			 * @brief Constructor for the JSON Object.
+			 *
+			 * Initializes a JSON object, optionally with a name. The object starts uninitialized and can be
+			 * configured to hold various types of JSON data (e.g., units, objects, arrays).
+			 *
+			 * @param p_name The name of the JSON object, defaulting to "Unnamed".
+			 */
+			Object(const std::string &p_name = "Unnamed");
+
+			/**
+			 * @brief Resets the JSON object to an uninitialized state.
+			 *
+			 * Clears the content of the JSON object and marks it as uninitialized. This is useful for reusing
+			 * the object for new JSON data.
+			 */
 			void reset();
 
+			/**
+			 * @brief Checks if the JSON object is configured as an object.
+			 *
+			 * @return True if the JSON object is structured as an object, false otherwise.
+			 */
 			bool isObject() const;
+
+			/**
+			 * @brief Checks if the JSON object is configured as an array.
+			 *
+			 * @return True if the JSON object is structured as an array, false otherwise.
+			 */
 			bool isArray() const;
+
+			/**
+			 * @brief Checks if the JSON object is a basic unit (e.g., bool, long, double, string, null).
+			 *
+			 * @return True if the JSON object represents a basic JSON unit, false otherwise.
+			 */
 			bool isUnit() const;
 
-			Object& addAttribute(const std::string& p_key);
+			/**
+			 * @brief Adds a new attribute to the JSON object.
+			 *
+			 * If the JSON object is not already an object type, this method will initialize it as an object.
+			 * Adds a new key-value pair where the value is a new, uninitialized JSON object.
+			 *
+			 * @param p_key The key under which to add the new attribute.
+			 * @return A reference to the newly added JSON object attribute.
+			 */
+			Object &addAttribute(const std::string &p_key);
 
-			const std::map<std::string, Object*>& members() const;
-			bool contains(const std::string& p_key) const;
-			Object& operator[](const std::string& p_key);
-			const Object& operator[](const std::string& p_key) const;
+			/**
+			 * @brief Retrieves a map of all members in the JSON object.
+			 *
+			 * Only applicable if the JSON object is of object type. Provides read-only access to the underlying
+			 * map representing member attributes.
+			 *
+			 * @return A const reference to the map containing the JSON object's members.
+			 */
+			const std::map<std::string, Object *> &members() const;
+
+			/**
+			 * @brief Checks if the JSON object contains a specific key.
+			 *
+			 * Only applicable if the JSON object is of object type. Checks whether the object has an attribute
+			 * with the specified key.
+			 *
+			 * @param p_key The key to check for existence.
+			 * @return True if the attribute exists, false otherwise.
+			 */
+			bool contains(const std::string &p_key) const;
+
+			/**
+			 * @brief Accesses a child object by key.
+			 *
+			 * Provides read-write access to a child object of this JSON object, identified by the given key.
+			 * If the key does not exist, a new JSON object is created and associated with that key.
+			 *
+			 * @param p_key The key of the child object to access.
+			 * @return A reference to the child JSON object.
+			 */
+			Object &operator[](const std::string &p_key);
+
+			/**
+			 * @brief Accesses a child object by key (const version).
+			 *
+			 * Provides read-only access to a child object of this JSON object, identified by the given key.
+			 * Throws an exception if the key does not exist.
+			 *
+			 * @param p_key The key of the child object to access.
+			 * @return A const reference to the child JSON object.
+			 */
+			const Object &operator[](const std::string &p_key) const;
+
+			/**
+			 * @brief Sets the JSON object to an object type.
+			 *
+			 * Initializes the JSON object as an empty object if it was not already. This method is useful
+			 * for explicitly declaring an object as a JSON object type, even without adding attributes.
+			 */
 			void setAsObject();
 
-			const std::vector<Object*>& asArray() const;
-			void resize(size_t p_size);
-			Object& append();
-			void push_back(Object& p_object);
-			Object& operator[](size_t p_index);
-			const Object& operator[](size_t p_index) const;
-			void setAsArray();
-			size_t size() const;
-			size_t count(const std::string& p_key) const;
+			/**
+			 * @brief Retrieves the JSON object as an array.
+			 *
+			 * Provides read-only access to the underlying vector representing the JSON array.
+			 * Only applicable if the JSON object is of array type.
+			 *
+			 * @return A const reference to the vector containing the JSON array elements.
+			 */
+			const std::vector<Object *> &asArray() const;
 
+			/**
+			 * @brief Resizes the JSON array.
+			 *
+			 * Adjusts the size of the JSON array. If the new size is greater than the current size, new elements are added.
+			 * If the JSON object is not already an array, this method initializes it as an empty array before resizing.
+			 *
+			 * @param p_size The new size of the JSON array.
+			 */
+			void resize(size_t p_size);
+
+			/**
+			 * @brief Appends a new, uninitialized JSON object to the array.
+			 *
+			 * Only applicable if the JSON object is of array type. Adds a new JSON object to the end of the array.
+			 *
+			 * @return A reference to the newly appended JSON object.
+			 */
+			Object &append();
+
+			/**
+			 * @brief Adds an existing JSON object to the end of the JSON array.
+			 *
+			 * Only applicable if the JSON object is of array type.
+			 *
+			 * @param p_object The JSON object to be added to the array.
+			 */
+			void push_back(Object &p_object);
+
+			/**
+			 * @brief Accesses an element in the JSON array by index.
+			 *
+			 * Provides read-write access to an element of the JSON array. If the index is out of bounds, throws an exception.
+			 *
+			 * @param p_index The index of the element to access.
+			 * @return A reference to the JSON object at the specified index.
+			 */
+			Object &operator[](size_t p_index);
+
+			/**
+			 * @brief Accesses an element in the JSON array by index (const version).
+			 *
+			 * Provides read-only access to an element of the JSON array. If the index is out of bounds, throws an exception.
+			 *
+			 * @param p_index The index of the element to access.
+			 * @return A const reference to the JSON object at the specified index.
+			 */
+			const Object &operator[](size_t p_index) const;
+
+			/**
+			 * @brief Sets the JSON object to an array type.
+			 *
+			 * Initializes the JSON object as an empty array if it was not already. Useful for explicitly declaring an object as a JSON array.
+			 */
+			void setAsArray();
+
+			/**
+			 * @brief Returns the size of the JSON array or object.
+			 *
+			 * If the JSON object is an array, returns the number of elements. If it is an object, returns the number of key-value pairs.
+			 *
+			 * @return The size of the JSON array or object.
+			 */
+			size_t size() const;
+
+			/**
+			 * @brief Counts occurrences of a key in the JSON object.
+			 *
+			 * Only applicable if the JSON object is of object type. Counts the number of times a specific key appears in the object.
+			 *
+			 * @param p_key The key to count occurrences of.
+			 * @return The number of occurrences of the specified key.
+			 */
+			size_t count(const std::string &p_key) const;
+
+			/**
+			 * @brief Checks if the JSON unit holds a value of the specified type.
+			 *
+			 * Validates whether the current JSON object, if it is a unit (a single value), contains a value of the specified template type.
+			 * Throws an exception if the JSON object is not a unit.
+			 *
+			 * @tparam TType The type to check against the stored value.
+			 * @return True if the unit holds a value of type TType, false otherwise.
+			 */
 			template <typename TType>
 			bool hold() const
 			{
@@ -84,9 +256,19 @@ namespace spk
 					throwException("Can't verify the holding type of an object who isn't a Unit");
 				return (std::holds_alternative<TType>(std::get<Unit>(_content)));
 			}
+
+			/**
+			 * @brief Sets the value of the JSON object to the specified value.
+			 *
+			 * Initializes or updates the JSON unit with the provided value. If the JSON object was not previously initialized as a unit,
+			 * it is initialized accordingly.
+			 *
+			 * @tparam TType The type of the value to set, excluding Object type to prevent recursion.
+			 * @param p_value The value to set in the JSON object.
+			 */
 			template <typename TType,
-				typename std::enable_if<!std::is_same<TType, Object>::value, int>::type = 0>
-			void set(const TType& p_value)
+					  typename std::enable_if<!std::is_same<TType, Object>::value, int>::type = 0>
+			void set(const TType &p_value)
 			{
 				if (_initialized == false)
 				{
@@ -96,18 +278,37 @@ namespace spk
 
 				std::get<Unit>(_content) = p_value;
 			}
+
+			/**
+			 * @brief Sets the JSON object to a copy of the provided Object value.
+			 *
+			 * Specialization for setting an Object type. Creates a new Object instance and assigns it to the JSON object.
+			 *
+			 * @tparam TType Must be Object type for this specialization to be invoked.
+			 * @param p_value The Object value to set in the JSON object.
+			 */
 			template <typename TType,
-				typename std::enable_if<std::is_same<TType, Object>::value, int>::type = 0>
-			void set(const TType& p_value)
+					  typename std::enable_if<std::is_same<TType, Object>::value, int>::type = 0>
+			void set(const TType &p_value)
 			{
-				Object* tmpObject = new Object(p_value);
-				set<Object*>(tmpObject);
+				Object *tmpObject = new Object(p_value);
+				set<Object *>(tmpObject);
 			}
+
+			/**
+			 * @brief Retrieves the stored value as the specified type.
+			 *
+			 * Accesses the value stored in the JSON unit as the specified type. Throws an exception if the actual type of the stored
+			 * value does not match the requested type.
+			 *
+			 * @tparam TType The type to cast the stored value to.
+			 * @return A const reference to the stored value cast to the specified type.
+			 */
 			template <typename TType>
-			const TType& as() const
+			const TType &as() const
 			{
-				const Unit& unit = std::get<Unit>(_content);
-				const TType* value = std::get_if<TType>(&unit);
+				const Unit &unit = std::get<Unit>(_content);
+				const TType *value = std::get_if<TType>(&unit);
 
 				if (value == nullptr)
 				{
@@ -119,11 +320,17 @@ namespace spk
 				return (*value);
 			}
 
-			void printUnit(std::ostream& p_os) const;
-			void printObject(std::ostream& p_os) const;
-			void printArray(std::ostream& p_os) const;
-
-			friend std::ostream& operator<<(std::ostream& p_os, const Object& p_object);
+			/**
+			 * @brief Outputs the JSON object to a standard output stream.
+			 *
+			 * Serializes the JSON object to a string and writes it to the provided output stream. Supports pretty printing based on
+			 * indentation settings of the JSON object.
+			 *
+			 * @param p_os The output stream to write the serialized JSON object to.
+			 * @param p_object The JSON object to serialize and write.
+			 * @return A reference to the updated output stream.
+			 */
+			friend std::ostream &operator<<(std::ostream &p_os, const Object &p_object);
 		};
 	}
 }
