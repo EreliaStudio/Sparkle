1c2363e90e9404cb95e5ae14e9312a14ee888b34
Hyarius <Boissy.Jeremy@yahoo.fr>
2024-02-25 11:07:31 +0100

    correcting stuff on orthographic matrix

--- Sparkle/src/math/spk_matrix4x4.cpp
+++ Sparkle/src/math/spk_matrix4x4.cpp
@@ -165,7 +165,7 @@ namespace spk
 
 		result.data[0][0] = 2.0f / (p_right - p_left);
 		result.data[1][1] = 2.0f / (p_top - p_bottom);
-		result.data[2][2] = -2.0f / (p_farPlane - p_nearPlane);
+		result.data[2][2] = -1.0f / (p_farPlane - p_nearPlane);
 		result.data[0][3] = -(p_right + p_left) / (p_right - p_left);
 		result.data[1][3] = -(p_top + p_bottom) / (p_top - p_bottom);
 		result.data[2][3] = -(p_farPlane + p_nearPlane) / (p_farPlane - p_nearPlane);
