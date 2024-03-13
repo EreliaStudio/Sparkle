999fc76ed70b0269eead4445863a27cc1210789e
Hyarius <Boissy.Jeremy@yahoo.fr>
2024-03-13 18:12:20 +0100

    Adding makeActiveChild method

--- Sparkle/src/widget/spk_basic_widgets.hpp
+++ Sparkle/src/widget/spk_basic_widgets.hpp
@@ -167,6 +167,8 @@ namespace spk::widget
          */
         Padding(const Config& p_config, IWidget* p_parent = nullptr);
 
+        Padding(const float& p_left, const float& p_right, const float& p_top, const float& p_bottom, IWidget* p_parent = nullptr);
+
         /**
          * Constructor for creating a padding by passing it the 4 values to create the configuration needed.
          * @param p_left The left composant of the padding.
