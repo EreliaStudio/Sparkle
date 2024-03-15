88865fd162700fe4f0d37fbc6362f21e13e5deb0
Hyarius <Boissy.Jeremy@yahoo.fr>
2024-03-15 10:15:36 +0100

    Adding the offset widget layout and commenting what was missing

--- Sparkle/src/widget/spk_single_child_widget.hpp
+++ Sparkle/src/widget/spk_single_child_widget.hpp
@@ -12,6 +12,10 @@ namespace spk::widget
     class SingleChildWidget : public IWidget
     {
     public:
+        /**
+         * Constructor for creating a widget without a name and set the parent widget.
+         * @param p_parent A pointer to the parent widget. This widget will be added as a child of the given parent.
+         */
         SingleChildWidget(IWidget* p_parent = nullptr);
         
         /**
