90714537d256b1d6976ea9db30dc25ca69f0d905
Jeremy Boissy <jeremy.boissy@mediasofts.fr>
2024-12-04 11:27:55 +0100

    Adding the base classes for network implementation

---
+++ src/structure/network/spk_client.cpp
@@ -0,0 +1,136 @@
+#include "structure/network/spk_client.hpp"
+
+namespace spk
+{
+	void Client::_receive()
+	{
+		while (_isConnected)
+		{
+			MessageObject message = _messagePool.obtain();
+			int headerSize = sizeof(spk::Message::Header);
+			int bytesRead = recv(_connectSocket, reinterpret_cast<char*>(&message->_header), headerSize, 0);
+			if (bytesRead == headerSize)
+			{
+				if (message->_header.length > 0)
+				{
+					message->resize(message->_header.length);
+					char* dataBuffer = reinterpret_cast<char*>(message->_buffer.data());
+					size_t totalBytesReceived = 0;
+					while (totalBytesReceived < message->_header.length)
+					{
+						bytesRead = recv(_connectSocket, dataBuffer + totalBytesReceived,
+										 static_cast<int>(message->_header.length) - static_cast<int>(totalBytesReceived), 0);
+						if (bytesRead <= 0)
+						{
+							break;
+						}
+						totalBytesReceived += bytesRead;
+					}
+				}
+				_messageQueue.push(std::move(message));
+			}
+			else if (bytesRead <= 0)
+			{
+				disconnect();
+				break;
+			}
+		}
+	}
+
+	Client::Client() :
+		_connectSocket(INVALID_SOCKET),
+		_isConnected(false),
+		_messageQueue(),
+		_onConnectCallback([&]() { std::cout << "Connected to the server" << std::endl; }),
+		_onDisconnectCallback([&]() { std::cout << "Disconnected from the server" << std::endl; })
+	{
+	}
+
+	Client::~Client()
+	{
+		disconnect();
+	}
+
+	bool Client::isConnected() const
+	{
+		return _isConnected;
+	}
+
+	void Client::connect(const std::string& p_address, size_t p_port)
+	{
+		WSADATA wsaData;
+		if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		{
+			throw std::runtime_error("Failed to initialize Winsock.");
+		}
+
+		_connectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+		if (_connectSocket == INVALID_SOCKET)
+		{
+			WSACleanup();
+			throw std::runtime_error("Cannot create socket.");
+		}
+
+		struct sockaddr_in serverAddress;
+		serverAddress.sin_family = AF_INET;
+		inet_pton(AF_INET, p_address.c_str(), &serverAddress.sin_addr);
+		serverAddress.sin_port = htons(static_cast<u_short>(p_port));
+
+		if (::connect(_connectSocket, reinterpret_cast<struct sockaddr*>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR)
+		{
+			closesocket(_connectSocket);
+			WSACleanup();
+			throw std::runtime_error("Failed to connect to server.");
+		}
+
+		_isConnected = true;
+		_onConnectCallback();
+		std::thread(&Client::_receive, this).detach();
+	}
+
+	void Client::disconnect()
+	{
+		if (_isConnected)
+		{
+			_isConnected = false;
+			closesocket(_connectSocket);
+			WSACleanup();
+			_onDisconnectCallback();
+		}
+	}
+
+	void Client::send(const Message& p_message)
+	{
+		if (_isConnected)
+		{
+			int headerSize = sizeof(spk::Message::Header);
+			int sentBytes = ::send(_connectSocket, reinterpret_cast<const char*>(&p_message.header()), headerSize, 0);
+
+			if (sentBytes != headerSize)
+			{
+				std::cerr << "Failed to send message header." << std::endl;
+				return;
+			}
+
+			if (p_message.header().length > 0)
+			{
+				sentBytes = ::send(_connectSocket, reinterpret_cast<const char*>(p_message.buffer().data()),
+								   static_cast<int>(p_message.header().length), 0);
+				if (sentBytes != static_cast<int>(p_message.header().length))
+				{
+					std::cerr << "Failed to send message data." << std::endl;
+					return;
+				}
+			}
+		}
+		else
+		{
+			throw std::runtime_error("Can't send a message through a non-connected client.");
+		}
+	}
+
+	spk::ThreadSafeQueue<Client::MessageObject>& Client::messages()
+	{
+		return _messageQueue;
+	}
+}
---
+++ src/structure/network/spk_message.cpp
@@ -0,0 +1,106 @@
+#include "structure/network/spk_message.hpp"
+#include <cstring>
+
+namespace spk
+{
+	Message::Header::Header() :
+		type(0),
+		emitterID(0),
+		length(0)
+	{
+		std::memset(reserved, 0, sizeof(reserved));
+	}
+
+	Message::Header::Header(Type p_type) :
+		type(p_type),
+		emitterID(0),
+		length(0)
+	{
+		std::memset(reserved, 0, sizeof(reserved));
+	}
+
+	Message::Header::Header(const ClientID &p_emitterID, const Type &p_type) :
+		type(p_type),
+		emitterID(p_emitterID),
+		length(0)
+	{
+		std::memset(reserved, 0, sizeof(reserved));
+	}
+
+	Message::Message() :
+		_header()
+	{
+		
+	}
+
+	Message::Message(const Header::Type &p_messageType) :
+		_header(p_messageType)
+	{
+
+	}
+
+	Message::Message(const Header::ClientID &p_clientToRedirectMessage, const Header::Type &p_messageType) :
+		_header(p_clientToRedirectMessage, p_messageType)
+	{
+
+	}
+
+	void Message::setType(const Header::Type &p_type)
+	{
+		_header.type = p_type;
+	}
+
+	void Message::setEmitterID(const Header::ClientID &p_emitterID)
+	{
+		_header.emitterID = p_emitterID;
+	}
+
+	void Message::resize(const size_t &p_newSize)
+	{
+		_buffer.resize(p_newSize);
+		_header.length = _buffer.size();
+	}
+
+	void Message::clear()
+	{
+		_buffer.clear();
+		_header.length = _buffer.size();
+	}
+
+	void Message::reset()
+	{
+		_buffer.reset();
+		_header.length = _buffer.size();
+	}
+
+	void Message::skip(const size_t &p_number)
+	{
+		_buffer.skip(p_number);
+	}
+
+	void Message::edit(const size_t &p_offset, const void *p_data, const size_t &p_dataSize)
+	{
+		_buffer.edit(p_offset, p_data, p_dataSize);
+	}
+
+	void Message::append(const void *p_data, const size_t &p_dataSize)
+	{
+		_buffer.append(p_data, p_dataSize);
+		_header.length = _buffer.size();
+	}
+
+	const Message::Header &Message::header() const
+	{
+		return _header;
+	}
+
+	const DataBuffer &Message::buffer() const
+	{
+		return _buffer;
+	}
+
+	size_t Message::size() const
+	{
+		return (sizeof(Header) + _buffer.size());
+	}
+}
