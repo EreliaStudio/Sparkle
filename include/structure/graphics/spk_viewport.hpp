bccfc9c739a986c3cf0a055a3dfed949a882074c
EreliaStudio <98489062+EreliaStudio@users.noreply.github.com>
2024-12-17 23:57:59 +0100

    adding a method to convert a point to the actual viewport matrix

--- include/structure/graphics/spk_viewport.hpp
+++ include/structure/graphics/spk_viewport.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "structure/graphics/spk_geometry_2D.hpp"
+#include "structure/math/spk_matrix.hpp"
 
 namespace spk
 {
@@ -8,6 +9,7 @@ namespace spk
 	{
 	private:
 		static inline float _maxLayer = 10000;
+		static inline Matrix4x4 _matrix = spk::Matrix4x4();
 		
 		Geometry2D _geometry;
 
@@ -19,5 +21,6 @@ namespace spk
 		void setGeometry(const Geometry2D& p_geometry);
 
 		void apply() const;
+		static spk::Vector3 convertScreenToOpenGL(const spk::Vector2Int p_screenPosition, const float& p_layer);
 	};
 }
--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -1,7 +1,39 @@
 #include "playground.hpp"
 
+class TestComponent : public spk::Component
+{
+private:
+
+public:
+	TestComponent() :
+		spk::Component(L"TestComponent")
+	{
+
+	}
+};
+
 int main()
 {
-	spk::cout << "Hello world" << std::endl;
-	return (0);
+	spk::GraphicalApplication app = spk::GraphicalApplication();
+
+	spk::SafePointer<spk::Window> win = app.createWindow(L"Playground", {{0, 0}, {840, 680}});
+
+	spk::GameEngine engine;
+
+	spk::Entity player(L"Player");
+	player.addComponent<TestComponent>();
+	
+	spk::Entity camera(L"Camera", &player);
+
+	spk::Entity cube(L"Cube");
+
+	engine.addEntity(&player);
+	engine.addEntity(&cube);
+
+	spk::GameEngineWidget gameEngineWidget = spk::GameEngineWidget(L"Engine widget", win->widget());
+	gameEngineWidget.setGeometry(win->geometry());
+	gameEngineWidget.setGameEngine(&engine);
+	gameEngineWidget.activate();
+
+	return (app.run());
 }
