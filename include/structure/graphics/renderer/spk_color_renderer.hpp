16a29407e931c8568e059f4e4afd9054b8bce3a4
EreliaStudio <98489062+EreliaStudio@users.noreply.github.com>
2024-12-18 23:55:21 +0100

    working on a chunk renderer

--- include/structure/graphics/renderer/spk_color_renderer.hpp
+++ include/structure/graphics/renderer/spk_color_renderer.hpp
@@ -3,6 +3,7 @@
 #include "structure/math/spk_vector2.hpp"
 #include "structure/math/spk_vector3.hpp"
 #include "structure/graphics/spk_color.hpp"
+#include "structure/graphics/spk_geometry_2D.hpp"
 #include "structure/graphics/opengl/spk_program.hpp"
 #include "structure/graphics/opengl/spk_buffer_set.hpp"
 #include "structure/graphics/opengl/spk_uniform_buffer_object.hpp"
@@ -28,83 +29,18 @@ namespace spk
 
         spk::Color _color;
 
-        void _initProgram()
-        {
-            const char* vertexShaderSrc = R"(#version 450
-            layout(location = 0) in vec2 inPosition;
-            layout(location = 1) in float inLayer;
-
-            layout(std140, binding = 0) uniform ColorData {
-                vec4 uColor;
-            };
-
-            void main()
-            {
-                // We can expand this if needed for transformations
-                gl_Position = vec4(inPosition, inLayer, 1.0);
-            }
-            )";
-
-            const char* fragmentShaderSrc = R"(#version 450
-            layout(std140, binding = 0) uniform ColorData {
-                vec4 uColor;
-            };
-
-            layout(location = 0) out vec4 outputColor;
-
-            void main()
-            {
-                outputColor = uColor;
-            }
-            )";
-
-            _program = spk::OpenGL::Program(vertexShaderSrc, fragmentShaderSrc);
-        }
-
-        void _initBuffers()
-        {
-            _bufferSet = spk::OpenGL::BufferSet({
-                {0, spk::OpenGL::LayoutBufferObject::Attribute::Type::Vector2}, // position
-                {1, spk::OpenGL::LayoutBufferObject::Attribute::Type::Float}    // layer
-            });
-
-            _colorUbo = spk::OpenGL::UniformBufferObject("ColorData", 0, 16);
-            _colorUbo.addElement("uColor", 0, 16);
-        }
+        void _initProgram();
+        void _initBuffers();
 
     public:
-        ColorRenderer(const spk::Color& p_color = spk::Color(255, 255, 255))
-        {
-            _initProgram();
-            _initBuffers();
-			setColor(p_color);
-        }
+        ColorRenderer(const spk::Color& p_color = spk::Color(255, 255, 255));
 
-        void setColor(const spk::Color& p_color)
-        {
-            _color = p_color;
+        void setColor(const spk::Color& p_color);
 
-            _colorUbo["uColor"] = _color;
-            _colorUbo.validate();
-        }
-
-        void clear()
-        {
-            _bufferSet.layout().clear();
-            _bufferSet.indexes().clear();
-        }
+        void clear();
 		
-		ColorRenderer& operator<<(const Vertex& p_element)
-		{
-			_bufferSet.layout() << p_element;
-			return *this;
-		}
-		
-		ColorRenderer& operator<<(const unsigned int& p_index)
-		{
-			_bufferSet.indexes() << p_index;
-			return *this;
-		}
+		ColorRenderer& operator<<(const Vertex& p_element);
+		ColorRenderer& operator<<(const unsigned int& p_index);
 
 		template<typename Container>
 		requires  requires (const Container& c) {
@@ -128,43 +64,10 @@ namespace spk
 			return *this;
 		}
 
-		void prepareSquare(const spk::Geometry2D& geom, float layer)
-		{
-			size_t nbVertex = _bufferSet.layout().size() / sizeof(Vertex);
+		void prepareSquare(const spk::Geometry2D& geom, float layer);
 
-			spk::Vector3 topLeft = spk::Viewport::convertScreenToOpenGL({geom.anchor.x, geom.anchor.y}, layer);
-			spk::Vector3 bottomRight = spk::Viewport::convertScreenToOpenGL({geom.anchor.x + static_cast<int32_t>(geom.size.x),
-																			geom.anchor.y + static_cast<int32_t>(geom.size.y)}, layer);
+		void validate();
 
-			*this << Vertex{ { topLeft.x,      bottomRight.y }, topLeft.z }
-				  << Vertex{ { bottomRight.x,  bottomRight.y }, topLeft.z }
-				  << Vertex{ { topLeft.x,      topLeft.y     }, topLeft.z }
-				  << Vertex{ { bottomRight.x,  topLeft.y     }, topLeft.z };
-
-			std::array<unsigned int, 6> indices = {0, 1, 2, 2, 1, 3};
-			for (const auto& index : indices)
-			{
-				*this << index + nbVertex;
-			}
-		}
-
-		void validate()
-		{
-            _bufferSet.layout().validate();
-            _bufferSet.indexes().validate();
-		}
-
-        void render()
-        {
-            _program.activate();
-            _bufferSet.activate();
-            _colorUbo.activate();
-
-            _program.render(_bufferSet.indexes().nbIndexes(), 1);
-
-            _colorUbo.deactivate();
-            _bufferSet.deactivate();
-            _program.deactivate();
-        }
+        void render();
     };
 }
--- include/structure/graphics/renderer/spk_font_renderer.hpp
+++ include/structure/graphics/renderer/spk_font_renderer.hpp
@@ -42,156 +42,23 @@ namespace spk
         spk::Color _glyphColor = spk::Color(255, 255, 255, 255);    // white by default
         spk::Color _outlineColor = spk::Color(0, 0, 0, 255);       // black by default
 
-        void _initProgram()
-        {
-            // Vertex Shader
-            const char* vertexShaderSrc = R"(
-            #version 450
+        void _initProgram();
+        void _initBuffers();
 
-            layout (location = 0) in vec2 inPosition;
-            layout (location = 1) in float inLayer;
-            layout (location = 2) in vec2 inUV;
-            layout (location = 0) out vec2 fragmentUVs;
-
-            layout(std140, binding = 0) uniform TextInformations
-            {
-                vec4 glyphColor;
-                vec4 outlineColor;
-            };
-
-            uniform sampler2D diffuseTexture;
-
-            void main()
-            {
-                gl_Position = vec4(inPosition, inLayer, 1.0f);
-                fragmentUVs = inUV;
-            }
-            )";
-
-            // Fragment Shader
-            const char* fragmentShaderSrc = R"(
-            #version 450
-
-            layout (location = 0) in vec2 fragmentUVs;
-            layout (location = 0) out vec4 outputColor;
-
-            layout(std140, binding = 0) uniform TextInformations
-            {
-                vec4 glyphColor;
-                vec4 outlineColor;
-            };
-
-            uniform sampler2D diffuseTexture;
-
-            float computeFormula(float x, float k)
-            {
-                return (1.0 - (exp(-k * x))) / (1.0 - exp(-k));
-            }
-
-            void main()
-            {
-                float grayscale = texture(diffuseTexture, fragmentUVs).r;
-
-                if (grayscale == 0)
-                {
-                    discard;
-                }
-
-                float outlineToGlyphThreshold = 0.2f;
-                if (grayscale <= outlineToGlyphThreshold)
-                {
-                    outputColor = outlineColor;
-                    outputColor.a = computeFormula(smoothstep(0, outlineToGlyphThreshold, grayscale), 20);
-                }
-                else
-                {
-                    float t = computeFormula(smoothstep(outlineToGlyphThreshold, 1.0, grayscale), -20);
-                    outputColor = mix(outlineColor, glyphColor, t);
-                }
-            }
-            )";
-
-            _program = spk::OpenGL::Program(vertexShaderSrc, fragmentShaderSrc);
-        }
-
-        void _initBuffers()
-        {
-            _bufferSet = spk::OpenGL::BufferSet({
-                {0, spk::OpenGL::LayoutBufferObject::Attribute::Type::Vector2}, // inPosition
-                {1, spk::OpenGL::LayoutBufferObject::Attribute::Type::Float},  // inLayer
-                {2, spk::OpenGL::LayoutBufferObject::Attribute::Type::Vector2}  // inUV
-            });
-
-            _samplerObject = spk::OpenGL::SamplerObject("diffuseTexture", spk::OpenGL::SamplerObject::Type::Texture2D, 0);
-
-            // The UBO layout:
-            // vec4 glyphColor;   // offset 0
-            // vec4 outlineColor; // offset 16
-            // total size: 32 bytes
-
-            _textInformationsUbo = spk::OpenGL::UniformBufferObject("TextInformations", 0, 32);
-            _textInformationsUbo.addElement("glyphColor", 0, sizeof(spk::Color)); 
-            _textInformationsUbo.addElement("outlineColor", 16, sizeof(spk::Color));
-        }
-
-        void _updateUbo()
-        {
-            _textInformationsUbo["glyphColor"] = _glyphColor;
-            _textInformationsUbo["outlineColor"] = _outlineColor;
-            _textInformationsUbo.validate();
-        }
+        void _updateUbo();
 
     public:
-        FontRenderer()
-        {
-            _initProgram();
-            _initBuffers();
-            _updateUbo();
-        }
+        FontRenderer();
 
-        void setFont(Font* p_font)
-        {
-            _font = p_font;
-            _atlas = &_font->atlas(_fontSize);
-            _samplerObject.bind(_atlas);
-        }
+        void setFont(Font* p_font);
+        void setFontSize(const Font::Size& p_fontSize);
+        void setGlyphColor(const spk::Color& p_color);
+        void setOutlineColor(const spk::Color& p_color);
 
-        void setFontSize(const Font::Size& p_fontSize)
-        {
-            _fontSize = p_fontSize;
-            _atlas = &_font->atlas(_fontSize);
-            _samplerObject.bind(_atlas);
-        }
+        void clear();
 
-        void setGlyphColor(const spk::Color& p_color)
-        {
-            _glyphColor = p_color;
-            _updateUbo();
-        }
-
-        void setOutlineColor(const spk::Color& p_color)
-        {
-            _outlineColor = p_color;
-            _updateUbo();
-        }
-
-        void clear()
-        {
-            _bufferSet.layout().clear();
-            _bufferSet.indexes().clear();
-        }
-
-        FontRenderer& operator<<(const Vertex& p_element)
-        {
-            _bufferSet.layout() << p_element;
-            return *this;
-        }
-
-        FontRenderer& operator<<(const unsigned int& p_index)
-        {
-            _bufferSet.indexes() << p_index;
-            return *this;
-        }
+        FontRenderer& operator<<(const Vertex& p_element);
+        FontRenderer& operator<<(const unsigned int& p_index);
 
         template<typename Container>
         requires requires (const Container& c) {
@@ -215,65 +82,10 @@ namespace spk
             return *this;
         }
 
-		void prepare(const std::wstring& text, const spk::Vector2Int& anchor, float layer)
-		{
-			spk::Vector2Int glyphAnchor = anchor;
-			unsigned int baseIndexes = 0;
+		void prepare(const std::wstring& text, const spk::Vector2Int& anchor, float layer);
 
-			for (wchar_t c : text)
-			{
-				const spk::Font::Glyph& glyph = _atlas->glyph(c);
+        void validate();
 
-				for (size_t i = 0; i < 4; i++)
-				{
-					Vertex newVertex;
-
-					Vector3 convertedPoint = spk::Viewport::convertScreenToOpenGL(glyphAnchor + glyph.positions[i], layer);
-
-					newVertex.position = convertedPoint.xy();
-					newVertex.uv = glyph.UVs[i];
-					newVertex.layer = convertedPoint.z;
-
-					*this << newVertex;
-				}
-
-				glyphAnchor += glyph.step;
-
-				for (size_t i = 0; i < 6; i++)
-				{
-					*this << (baseIndexes + spk::Font::Glyph::indexesOrder[i]);
-				}
-
-				baseIndexes += 4;
-			}
-
-			validate();
-		}
-
-        void validate()
-        {
-            _bufferSet.layout().validate();
-            _bufferSet.indexes().validate();
-        }
-
-        void render()
-        {
-            if (_atlas == nullptr)
-			{
-			    return;
-			}
-
-            _program.activate();
-            _bufferSet.activate();
-            _samplerObject.activate();
-            _textInformationsUbo.activate();
-
-            _program.render(_bufferSet.indexes().nbIndexes(), 1);
-
-            _textInformationsUbo.deactivate();
-            _samplerObject.deactivate();
-            _bufferSet.deactivate();
-            _program.deactivate();
-        }
+        void render();
     };
 }
