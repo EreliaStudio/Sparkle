ee80d60ef3930ba56d8f546d7f2560070f8875e3
EreliaStudio <98489062+EreliaStudio@users.noreply.github.com>
2025-02-23 10:48:17 +0100

    Add activation and deactivation callbacks to Component; filter event handling by active state in Entity

--- src/structure/engine/spk_entity.cpp
+++ src/structure/engine/spk_entity.cpp
@@ -187,7 +187,10 @@ namespace spk
 
 		for (auto& component : _components)
 		{
-			component->onUpdateEvent(p_event);
+			if (component->isActive() == true)
+			{
+				component->onUpdateEvent(p_event);
+			}
 		}
 	}
 	
@@ -209,7 +212,10 @@ namespace spk
 
 		for (auto& component : _components)
 		{
-			component->onKeyboardEvent(p_event);
+			if (component->isActive() == true)
+			{
+				component->onKeyboardEvent(p_event);
+			}
 		}
 	}
 	
@@ -231,7 +237,10 @@ namespace spk
 
 		for (auto& component : _components)
 		{
-			component->onMouseEvent(p_event);
+			if (component->isActive() == true)
+			{
+				component->onMouseEvent(p_event);
+			}
 		}
 	}
 
@@ -253,8 +262,12 @@ namespace spk
 
 		for (auto& component : _components)
 		{
-			component->onControllerEvent(p_event);
+			if (component->isActive() == true)
+			{
+				component->onControllerEvent(p_event);
+			}
 		}
+			
 	}
 	
 	void Entity::onTimerEvent(spk::TimerEvent& p_event)
@@ -275,7 +288,10 @@ namespace spk
 
 		for (auto& component : _components)
 		{
-			component->onTimerEvent(p_event);
+			if (component->isActive() == true)
+			{
+				component->onTimerEvent(p_event);
+			}
 		}
 	}
 
