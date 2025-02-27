f7442ab5b3ddbdfb0596f5f9ecf5b21799d41be6
Hyarius <Hyarius@erelia-studio.com>
2025-02-27 17:17:07 +0100

    Refactor Pool class to use vector for storage, improve thread safety, and enhance memory management

--- include/structure/container/spk_pool.hpp
+++ include/structure/container/spk_pool.hpp
@@ -1,125 +1,170 @@
 #pragma once
 
-#include <deque>
+#include <vector>
 #include <memory>
 #include <functional>
 #include <mutex>
 #include <utility>
+#include <limits>
 
 namespace spk
 {
 	template <typename TType>
-	class Pool : public std::enable_shared_from_this<Pool<TType>>
+	class Pool
 	{
 	public:
-		using Destructor = std::function<void(TType *)>;
-		using Object = std::unique_ptr<TType, Destructor>;
+		using Deleter = std::function<void(TType *)>;
+		using Object = std::unique_ptr<TType, Deleter>;
 
 	private:
-		mutable std::mutex _mutex;
-
-		size_t _preallocationLimit = std::numeric_limits<size_t>::max();
-		std::deque<std::unique_ptr<TType>> _preallocatedElements;
-
-		void _recycle(TType *ptr)
+		struct Data
 		{
-			std::lock_guard<std::mutex> lock(_mutex);
-			if (_preallocatedElements.size() < _preallocationLimit)
-			{
-				_preallocatedElements.emplace_back(ptr);
-			}
-			else
-			{
-				delete ptr;
-			}
-		}
+			std::mutex _mutex;
+			std::vector<std::unique_ptr<TType>> _availableElements;
+			size_t _maximumSize = std::numeric_limits<size_t>::max();
 
-		void _insert(TType *ptr)
-		{
-			std::lock_guard<std::mutex> lock(_mutex);
+			bool _closed = false;
+		};
 
-			std::unique_ptr<TType> bindedPtr = std::unique_ptr<TType>(ptr);
-
-			if (_preallocatedElements.size() < _preallocationLimit)
-			{
-				_preallocatedElements.emplace_back(std::move(bindedPtr));
-			}
-		}
+		std::shared_ptr<Data> _data;
 
 	public:
-		Pool() = default;
-		~Pool() = default;
-
-		void setMaximumAllocatedSize(const size_t &p_size)
+		Pool() :
+			_data(std::make_shared<Data>())
 		{
-			std::lock_guard<std::mutex> lock(_mutex);
-			_preallocationLimit = p_size;
+		}
 
-			while (_preallocatedElements.size() > _preallocationLimit)
+		~Pool()
+		{
+			std::scoped_lock<std::mutex> lock(_data->_mutex);
+			_data->_closed = true;
+		}
+
+		Pool(const Pool &) = delete;
+		Pool &operator=(const Pool &) = delete;
+
+		void setMaximumAllocationSize(size_t p_size)
+		{
+			std::scoped_lock<std::mutex> lock(_data->_mutex);
+
+			if (_data->_closed == true)
 			{
-				_preallocatedElements.pop_back();
+				throw std::runtime_error("Can't edit the maximulm allocated size of a closed pool");
+			}
+
+			_data->_maximumSize = p_size;
+
+			while (_data->_availableElements.size() > _data->_maximumSize)
+			{
+				_data->_availableElements.pop_back();
 			}
 		}
 
 		size_t size() const
 		{
-			std::lock_guard<std::mutex> lock(_mutex);
-			return _preallocatedElements.size();
+			std::scoped_lock<std::mutex> lock(_data->_mutex);
+			return _data->_availableElements.size();
 		}
 
 		template <typename... TArgs>
 		void allocate(TArgs &&...args)
 		{
-			std::lock_guard<std::mutex> lock(_mutex);
-			_insert(new TType(std::forward<TArgs>(args)...));
+			std::scoped_lock<std::mutex> lock(_data->_mutex);
+
+			if (_data->_closed == true)
+			{
+				throw std::runtime_error("Can't allocate a new object in closed pool");
+			}
+
+			_data->_availableElements.emplace_back(
+				std::make_unique<TType>(std::forward<TArgs>(args)...));
 		}
 
 		template <typename... TArgs>
 		void resize(size_t p_newSize, TArgs &&...args)
 		{
-			std::lock_guard<std::mutex> lock(_mutex);
-			while (_preallocatedElements.size() < p_newSize)
+			std::scoped_lock<std::mutex> lock(_data->_mutex);
+
+			if (_data->_closed == true)
 			{
-				_insert(new TType(std::forward<TArgs>(args)...));
+				throw std::runtime_error("Can't resize a closed pool");
 			}
+
+			while (_data->_availableElements.size() > p_newSize)
+			{
+				_data->_availableElements.pop_back();
+			}
+
+			while (_data->_availableElements.size() < p_newSize)
+			{
+				_data->_availableElements.emplace_back(
+					std::make_unique<TType>(std::forward<TArgs>(args)...));
+			}
+		}
+
+		void release()
+		{
+			std::scoped_lock<std::mutex> lock(_data->_mutex);
+
+			if (_data->_closed == true)
+			{
+				throw std::runtime_error("Can't release a closed pool");
+			}
+
+			_data->_availableElements.clear();
 		}
 
 		template <typename... TArgs>
 		Object obtain(TArgs &&...args)
 		{
-			TType *tmpPointer;
-
 			{
-				std::lock_guard<std::mutex> lock(_mutex);
-				if (_preallocatedElements.empty())
-				{
-					tmpPointer = new TType(std::forward<TArgs>(args)...);
-				}
-				else
-				{
-					tmpPointer = _preallocatedElements.front().release();
+				std::scoped_lock<std::mutex> lock(_data->_mutex);
 
-					_preallocatedElements.pop_front();
-
-					tmpPointer->~TType();
-					new (tmpPointer) TType(std::forward<TArgs>(args)...);
+				if (_data->_closed == true)
+				{
+					throw std::runtime_error("Can't obtain object from a closed pool");
 				}
 			}
 
-			std::weak_ptr<Pool<TType>> selfWeak = this->shared_from_this();
-			auto deleter = [selfWeak](TType *ptr)
+			TType *rawPtr = nullptr;
+
 			{
-				if (auto selfShared = selfWeak.lock())
+				std::scoped_lock<std::mutex> lock(_data->_mutex);
+
+				if (_data->_availableElements.empty())
 				{
-					selfShared->_recycle(p);
+					rawPtr = new TType(std::forward<TArgs>(args)...);
 				}
 				else
 				{
-					delete p;
+					rawPtr = _data->_availableElements.back().release();
+					_data->_availableElements.pop_back();
+
+					rawPtr->~TType();
+					new (rawPtr) TType(std::forward<TArgs>(args)...);
 				}
+			}
+
+			std::weak_ptr<Data> weakData = _data;
+
+			auto deleter = [weakData](TType *ptr)
+			{
+				if (auto data = weakData.lock())
+				{
+					std::scoped_lock<std::mutex> lock(data->_mutex);
+
+					if (data->_closed == false &&
+						data->_availableElements.size() < data->_maximumSize)
+					{
+						data->_availableElements.emplace_back(ptr);
+						return;
+					}
+				}
+
+				delete ptr;
 			};
 
-			return (Object(tmpPointer, deleter));
+			return Object(rawPtr, deleter);
 		}
 	};
 }
