148a75d4f41703113797d705548c151b5563ed2f
Erelia Studio <hyarius@erelia-studio.com>
2025-05-10 12:57:32 +0200

    Add SpacerWidget class (#22)

--- include/sparkle.hpp
+++ include/sparkle.hpp
@@ -114,3 +114,4 @@
 #include "widget/spk_grid_layout.hpp"
 #include "widget/spk_linear_layout.hpp"
 #include "widget/spk_form_layout.hpp"
+#include "widget/spk_spacer_widget.hpp"
---
+++ include/widget/spk_spacer_widget.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "widget/spk_widget.hpp"
+#include "structure/math/spk_vector2.hpp"
+
+namespace spk
+{
+    class SpacerWidget : public Widget
+    {
+    private:
+        spk::Vector2UInt _minimalSize = {1, 1};
+        spk::Vector2UInt _maximalSize = {
+				std::numeric_limits<uint32_t>::max(),
+				std::numeric_limits<uint32_t>::max()
+			};
+
+    public:
+        SpacerWidget(const std::wstring &p_name, spk::SafePointer<spk::Widget> p_parent) :
+            Widget(p_name, p_parent)
+        {
+        }
+		
+        ~SpacerWidget()
+        {
+        }
+
+		void setMinimalSize(const spk::Vector2UInt &p_size)
+		{
+			_minimalSize = p_size;
+		}
+
+		void setMaximalSize(const spk::Vector2UInt &p_size)
+		{
+			_maximalSize = p_size;
+		}
+
+        spk::Vector2UInt minimalSize() const override
+        {
+            return _minimalSize;
+        }
+
+        spk::Vector2UInt maximalSize() const override
+        {
+            return _maximalSize;
+        }
+    };
+}
