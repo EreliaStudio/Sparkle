7ce7575deed7135bc50fbca843373ecac1780037
Hyarius <Hyarius@erelia-studio.com>
2025-06-18 12:23:06 +0200

    Update UpdateEvent structure

--- include/application/module/spk_update_module.hpp
+++ include/application/module/spk_update_module.hpp
@@ -15,10 +15,18 @@ namespace spk
 		long long _lastMilliseconds = 0;
 		double _lastSeconds = 0.0f;
 
+		spk::SafePointer<const spk::Mouse> _mouse = nullptr;
+		spk::SafePointer<const spk::Keyboard> _keyboard = nullptr;
+		spk::SafePointer<const spk::Controller> _controller = nullptr;
+
 		void _treatEvent(spk::UpdateEvent&& p_event) override;
 		spk::UpdateEvent _convertEventToEventType(spk::Event&& p_event) override;
 
 	public:
 		UpdateModule(spk::SafePointer<spk::Widget> p_widget);
+
+		void bind(const spk::SafePointer<const Keyboard>& p_keyboard);
+		void bind(const spk::SafePointer<const Mouse>& p_mouse);
+		void bind(const spk::SafePointer<const Controller>& p_controller);
 	};
 }
