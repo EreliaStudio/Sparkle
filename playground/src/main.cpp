54acc826f7588334b762b07fc18d9c8d73621eca
Hyarius <Hyarius@erelia-studio.com>
2025-07-12 18:25:04 +0200

    Keeping the comment token instead of removing them, and keeping the space to keep the correct value of col and line

--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -5,33 +5,33 @@
 int main()
 {
 	std::wstring luminaCode = LR"(
-		#include <test>
+#include <test>
 
-		Input -> VertexPass : Vector2 modelPosition;
-		Input -> VertexPass : Color modelColor;
+Input -> VertexPass : Vector2 modelPosition;
+Input -> VertexPass : Color modelColor;
 
-		VertexPass -> FragmentPass : Color fragmentColor;
+VertexPass -> FragmentPass : Color fragmentColor;
 
-		VertexPass()
-		{
-			fragmentColor = modelColor; // End line comment
-			pixelPosition = vec4(modelPosition, 0.0, 1.0);
-		}
+VertexPass()
+{
+	fragmentColor = modelColor; // End line comment
+	pixelPosition = vec4(modelPosition, 0.0, 1.0);
+}
 
-		/*
-		
-		Multiline commentary
-		*/
+/*
 
-		FragmentPass()
-		{
-			if (fragmentColor.a == 0)
-			{
-				discard;
-			}
+Multiline commentary
+*/
 
-			pixelColor = fragmentColor;
-		}
+FragmentPass()
+{
+	if (fragmentColor.a == 0)
+	{
+		discard;
+	}
+
+	pixelColor = fragmentColor;
+}
 	)";
 
 	spk::Lumina::Compiler compiler;
