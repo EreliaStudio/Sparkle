7cd9b441e906555e9f4fb8d7adc0996464bfaa99
Erelia Studio <hyarius@erelia-studio.com>
2025-07-14 22:14:40 +0200

    Handle include directives in lexer

--- src/structure/graphics/lumina/compiler/spk_ast.cpp
+++ src/structure/graphics/lumina/compiler/spk_ast.cpp
@@ -1,6 +1,150 @@
 #include "structure/graphics/lumina/compiler/spk_ast.hpp"
+#include "utils/spk_string_utils.hpp"
 
 namespace spk::Lumina
 {
-	
+       std::wstring to_wstring(ASTNode::Kind p_kind)
+       {
+               switch (p_kind)
+               {
+                       case ASTNode::Kind::Token:                return L"Token";
+                       case ASTNode::Kind::Compound:             return L"Compound";
+                       case ASTNode::Kind::Namespace:            return L"Namespace";
+                       case ASTNode::Kind::Structure:            return L"Structure";
+                       case ASTNode::Kind::AttributeBlock:       return L"AttributeBlock";
+                       case ASTNode::Kind::ConstantBlock:        return L"ConstantBlock";
+                       case ASTNode::Kind::Texture:              return L"Texture";
+                       case ASTNode::Kind::Include:              return L"Include";
+                       case ASTNode::Kind::Function:             return L"Function";
+                       case ASTNode::Kind::Method:               return L"Method";
+                       case ASTNode::Kind::PipelineDefinition:   return L"PipelineDefinition";
+                       case ASTNode::Kind::PipelineBody:         return L"PipelineBody";
+                       default:                                  return L"(Invalid)";
+               }
+       }
+
+       std::string to_string(ASTNode::Kind p_kind)
+       {
+               return spk::StringUtils::wstringToString(to_wstring(p_kind));
+       }
+       void TokenNode::print(std::wostream &p_os, size_t p_indent) const
+       {
+               p_os << std::wstring(p_indent, L' ') << L"Token("
+                       << to_wstring(token.type) << L", \"" << token.lexeme << L"\")\n";
+       }
+
+       void CompoundNode::print(std::wostream &p_os, size_t p_indent) const
+       {
+               p_os << std::wstring(p_indent, L' ') << L"Compound{" << std::endl;
+               for (const auto &child : children)
+               {
+                       if (child)
+                       {
+                               child->print(p_os, p_indent + 2);
+                       }
+               }
+               p_os << std::wstring(p_indent, L' ') << L"}" << std::endl;
+       }
+
+       void NamespaceNode::print(std::wostream &p_os, size_t p_indent) const
+       {
+               p_os << std::wstring(p_indent, L' ') << L"Namespace " << name.lexeme << L"{" << std::endl;
+               if (body)
+               {
+                       body->print(p_os, p_indent + 2);
+               }
+               p_os << std::wstring(p_indent, L' ') << L"}" << std::endl;
+       }
+
+       void StructureNode::print(std::wostream &p_os, size_t p_indent) const
+       {
+               p_os << std::wstring(p_indent, L' ') << L"Structure " << name.lexeme << L"{" << std::endl;
+               if (body)
+               {
+                       body->print(p_os, p_indent + 2);
+               }
+               p_os << std::wstring(p_indent, L' ') << L"}" << std::endl;
+       }
+
+       void AttributeBlockNode::print(std::wostream &p_os, size_t p_indent) const
+       {
+               p_os << std::wstring(p_indent, L' ') << L"AttributeBlock " << name.lexeme << L"{" << std::endl;
+               if (body)
+               {
+                       body->print(p_os, p_indent + 2);
+               }
+               p_os << std::wstring(p_indent, L' ') << L"}" << std::endl;
+       }
+
+       void ConstantBlockNode::print(std::wostream &p_os, size_t p_indent) const
+       {
+               p_os << std::wstring(p_indent, L' ') << L"ConstantBlock " << name.lexeme << L"{" << std::endl;
+               if (body)
+               {
+                       body->print(p_os, p_indent + 2);
+               }
+               p_os << std::wstring(p_indent, L' ') << L"}" << std::endl;
+       }
+
+       void TextureNode::print(std::wostream &p_os, size_t p_indent) const
+       {
+               p_os << std::wstring(p_indent, L' ') << L"Texture " << name.lexeme << L";" << std::endl;
+       }
+
+       void FunctionNode::print(std::wostream &p_os, size_t p_indent) const
+       {
+               p_os << std::wstring(p_indent, L' ') << to_wstring(kind);
+               for (const auto &tok : header)
+               {
+                       p_os << L" " << tok.lexeme;
+               }
+               if (body)
+               {
+                       p_os << L" {" << std::endl;
+                       body->print(p_os, p_indent + 2);
+                       p_os << std::wstring(p_indent, L' ') << L"}" << std::endl;
+               }
+               else
+               {
+                       p_os << L";" << std::endl;
+               }
+       }
+
+       void PipelineDefinitionNode::print(std::wostream &p_os, size_t p_indent) const
+       {
+               p_os << std::wstring(p_indent, L' ') << L"Pipeline " << fromStage.lexeme << L" -> " << toStage.lexeme << L":";
+               for (const auto &tok : declaration)
+               {
+                       p_os << L" " << tok.lexeme;
+               }
+               p_os << L";" << std::endl;
+       }
+
+       void PipelineBodyNode::print(std::wostream &p_os, size_t p_indent) const
+       {
+               p_os << std::wstring(p_indent, L' ') << L"PipelineStage " << stage.lexeme << L"(){" << std::endl;
+               if (body)
+               {
+                       body->print(p_os, p_indent + 2);
+               }
+               p_os << std::wstring(p_indent, L' ') << L"}" << std::endl;
+       }
+
+       void IncludeNode::print(std::wostream &p_os, size_t p_indent) const
+       {
+               p_os << std::wstring(p_indent, L' ') << L"Include ";
+               if (system)
+               {
+                       p_os << L"<";
+               }
+               for (const auto &tok : path)
+               {
+                       p_os << tok.lexeme;
+               }
+               if (system)
+               {
+                       p_os << L">";
+               }
+               p_os << std::endl;
+       }
 }
--- src/structure/graphics/lumina/compiler/spk_lexer.cpp
+++ src/structure/graphics/lumina/compiler/spk_lexer.cpp
@@ -7,14 +7,21 @@
 
 namespace spk::Lumina
 {
-	Lexer::Lexer(SourceManager &p_sourceManager, const std::vector<Token> &p_tokens) :
-		_sourceManager(p_sourceManager),
-		_tokens(p_tokens)
-	{
-		_dispatch = {
-
-		};
-	}
+        Lexer::Lexer(SourceManager &p_sourceManager, const std::vector<Token> &p_tokens) :
+                _sourceManager(p_sourceManager),
+                _tokens(p_tokens)
+        {
+                _dispatch = {
+                        {Token::Type::OpenCurlyBracket, &Lexer::parseCompound},
+                        {Token::Type::Namespace, &Lexer::parseNamespace},
+                        {Token::Type::Struct, &Lexer::parseStruct},
+                        {Token::Type::AttributeBlock, &Lexer::parseAttributeBlock},
+                        {Token::Type::ConstantBlock, &Lexer::parseConstantBlock},
+                        {Token::Type::Texture, &Lexer::parseTexture},
+                        {Token::Type::Preprocessor, &Lexer::parseInclude},
+                        {Token::Type::ShaderPass, &Lexer::parseShader}
+                };
+        }
 
 	const Token &Lexer::peek(std::ptrdiff_t p_offset) const
 	{
@@ -44,13 +51,190 @@ namespace spk::Lumina
 		return peek(-1);
 	}
 
-	void Lexer::skipComment()
-	{
-		while (peek().type == Token::Type::Comment || peek().type == Token::Type::Whitespace)
-		{
-			advance();
-		}
-	}
+       void Lexer::skipComment()
+       {
+               while (peek().type == Token::Type::Comment || peek().type == Token::Type::Whitespace)
+               {
+                       advance();
+               }
+       }
+
+       bool Lexer::isFunctionStart() const
+       {
+               return peek().type == Token::Type::Identifier &&
+                      peek(1).type == Token::Type::Identifier &&
+                      peek(2).type == Token::Type::OpenParenthesis;
+       }
+
+       std::unique_ptr<ASTNode> Lexer::parseFunction(ASTNode::Kind p_kind)
+       {
+               std::vector<Token> header;
+               while (!eof())
+               {
+                       if (peek().type == Token::Type::OpenCurlyBracket || peek().type == Token::Type::Semicolon)
+                       {
+                               break;
+                       }
+                       header.emplace_back(advance());
+               }
+
+               std::unique_ptr<CompoundNode> body;
+               if (peek().type == Token::Type::OpenCurlyBracket)
+               {
+                       auto compound = parseCompound();
+                       body.reset(static_cast<CompoundNode*>(compound.release()));
+               }
+
+               if (peek().type == Token::Type::Semicolon)
+               {
+                       advance();
+               }
+
+               return std::make_unique<FunctionNode>(p_kind, std::move(header), std::move(body));
+       }
+
+       std::unique_ptr<ASTNode> Lexer::parseNamespace()
+       {
+               Token nameTok = expect(Token::Type::Namespace);
+               Token ident = expect(Token::Type::Identifier);
+               auto bodyAst = parseCompound();
+               auto body = std::unique_ptr<CompoundNode>(static_cast<CompoundNode*>(bodyAst.release()));
+               return std::make_unique<NamespaceNode>(ident, std::move(body));
+       }
+
+       std::unique_ptr<ASTNode> Lexer::parseStruct()
+       {
+               Token kw = expect(Token::Type::Struct);
+               Token ident = expect(Token::Type::Identifier);
+               bool prev = _inStruct;
+               _inStruct = true;
+               auto bodyAst = parseCompound();
+               _inStruct = prev;
+               auto body = std::unique_ptr<CompoundNode>(static_cast<CompoundNode*>(bodyAst.release()));
+               expect(Token::Type::Semicolon, "Expected ';'");
+               return std::make_unique<StructureNode>(ident, std::move(body));
+       }
+
+       std::unique_ptr<ASTNode> Lexer::parseAttributeBlock()
+       {
+               Token kw = expect(Token::Type::AttributeBlock);
+               Token ident = expect(Token::Type::Identifier);
+               bool prev = _inStruct;
+               _inStruct = true;
+               auto bodyAst = parseCompound();
+               _inStruct = prev;
+               auto body = std::unique_ptr<CompoundNode>(static_cast<CompoundNode*>(bodyAst.release()));
+               expect(Token::Type::Semicolon, "Expected ';'");
+               return std::make_unique<AttributeBlockNode>(ident, std::move(body));
+       }
+
+       std::unique_ptr<ASTNode> Lexer::parseConstantBlock()
+       {
+               Token kw = expect(Token::Type::ConstantBlock);
+               Token ident = expect(Token::Type::Identifier);
+               bool prev = _inStruct;
+               _inStruct = true;
+               auto bodyAst = parseCompound();
+               _inStruct = prev;
+               auto body = std::unique_ptr<CompoundNode>(static_cast<CompoundNode*>(bodyAst.release()));
+               expect(Token::Type::Semicolon, "Expected ';'");
+               return std::make_unique<ConstantBlockNode>(ident, std::move(body));
+       }
+
+       std::unique_ptr<ASTNode> Lexer::parseTexture()
+       {
+               Token kw = expect(Token::Type::Texture);
+               Token ident = expect(Token::Type::Identifier);
+               expect(Token::Type::Semicolon, "Expected ';'");
+               (void)kw;
+               return std::make_unique<TextureNode>(ident);
+       }
+
+       std::unique_ptr<ASTNode> Lexer::parseInclude()
+       {
+               Token hash = expect(Token::Type::Preprocessor);
+               Token kw = expect(Token::Type::Include);
+
+               bool system = false;
+               std::vector<Token> path;
+
+               if (peek().type == Token::Type::Less)
+               {
+                       system = true;
+                       advance();
+                       while (!eof() && peek().type != Token::Type::Greater)
+                       {
+                               path.emplace_back(advance());
+                       }
+                       expect(Token::Type::Greater, "Expected '>'");
+               }
+               else
+               {
+                       path.emplace_back(expect({Token::Type::StringLiteral, Token::Type::Identifier}));
+               }
+
+               (void)kw;
+               return std::make_unique<IncludeNode>(std::move(path), system, hash.location);
+       }
+
+       std::unique_ptr<ASTNode> Lexer::parseShader()
+       {
+               Token first = expect(Token::Type::ShaderPass);
+               if (peek().type == Token::Type::Arrow)
+               {
+                       advance();
+                       Token second = expect(Token::Type::ShaderPass);
+                       expect(Token::Type::Colon);
+                       std::vector<Token> decl;
+                       while (!eof() && peek().type != Token::Type::Semicolon)
+                       {
+                               decl.emplace_back(advance());
+                       }
+                       expect(Token::Type::Semicolon, "Expected ';'");
+                       return std::make_unique<PipelineDefinitionNode>(first, second, std::move(decl));
+               }
+
+               expect(Token::Type::OpenParenthesis);
+               expect(Token::Type::CloseParenthesis);
+               auto bodyAst = parseCompound();
+               auto body = std::unique_ptr<CompoundNode>(static_cast<CompoundNode*>(bodyAst.release()));
+               return std::make_unique<PipelineBodyNode>(first, std::move(body));
+       }
+
+       std::unique_ptr<ASTNode> Lexer::parseGeneric()
+       {
+               if (isFunctionStart())
+               {
+                       ASTNode::Kind kind = _inStruct ? ASTNode::Kind::Method : ASTNode::Kind::Function;
+                       return parseFunction(kind);
+               }
+               return std::make_unique<TokenNode>(advance());
+       }
+
+       std::unique_ptr<ASTNode> Lexer::parseCompound()
+       {
+               Location loc = expect(Token::Type::OpenCurlyBracket).location;
+               auto node = std::make_unique<CompoundNode>(loc);
+               while (!eof())
+               {
+                       skipComment();
+                       if (peek().type == Token::Type::CloseCurlyBracket)
+                       {
+                               break;
+                       }
+
+                       auto it = _dispatch.find(peek().type);
+                       if (it == _dispatch.end())
+                       {
+                               node->children.emplace_back(parseGeneric());
+                               continue;
+                       }
+
+                       node->children.emplace_back((this->*it->second)());
+               }
+               expect(Token::Type::CloseCurlyBracket, "Expected '}'");
+               return node;
+       }
 
 	static std::string makeUnexpected(const Token &p_tok)
 	{
@@ -99,14 +283,16 @@ namespace spk::Lumina
 				break;
 			}
 
-			auto it = _dispatch.find(peek().type);
-			if (it == _dispatch.end())
-			{
-				throw TokenException{makeUnexpected(peek()), peek(), _sourceManager};
-			}
-
-			result.emplace_back((this->*it->second)());
-		}
-		return result;
-	}
+                       auto it = _dispatch.find(peek().type);
+                       if (it == _dispatch.end())
+                       {
+                               result.emplace_back(parseGeneric());
+                       }
+                       else
+                       {
+                               result.emplace_back((this->*it->second)());
+                       }
+               }
+               return result;
+       }
 }
