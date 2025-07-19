7b91ed37a2ebdc5a65b590578029bb6023b8db06
Erelia Studio <hyarius@erelia-studio.com>
2025-07-19 20:10:53 +0200

    Add type lookup and insertion helpers

--- include/structure/graphics/lumina/compiler/spk_shader_data.hpp
+++ include/structure/graphics/lumina/compiler/spk_shader_data.hpp
@@ -10,12 +10,13 @@
 namespace spk::Lumina
 {
     struct TypeSymbol;
+    struct NamespaceSymbol;
 
 struct Variable
     {
         std::wstring name;
         TypeSymbol* type = nullptr;
-    void print(std::wostream& os, size_t indent = 0) const;
+        void print(std::wostream& os, size_t indent = 0) const;
 };
 
     struct Statement
@@ -35,6 +36,12 @@ struct Variable
         std::wstring signature;
         std::vector<Statement> body;
         void print(std::wostream& os, size_t indent = 0) const;
+
+        FunctionSymbol() = default;
+        FunctionSymbol(const std::wstring& name,
+                       const TypeSymbol* returnType,
+                       const std::vector<Variable>& parameters,
+                       const std::vector<Statement>& body = {});
     };
 
 struct TypeSymbol
@@ -47,6 +54,7 @@ struct TypeSymbol
     };
 
     std::wstring name;
+    NamespaceSymbol* parent = nullptr;
     Role role = Role::Structure;
     std::unordered_set<TypeSymbol*> convertible;
     std::vector<Variable> members;
@@ -54,11 +62,25 @@ struct TypeSymbol
     std::vector<FunctionSymbol> methods;
     std::unordered_map<std::wstring, std::vector<FunctionSymbol>> operators;
     void print(std::wostream& os, size_t indent = 0) const;
-    };
+
+    TypeSymbol() = default;
+    TypeSymbol(const std::wstring& name,
+               Role role = Role::Structure,
+               NamespaceSymbol* parent = nullptr);
+
+    void addConstructor(const std::vector<Variable>& parameters);
+    void addMethod(TypeSymbol* returnType,
+                   const std::wstring& methodName,
+                   const std::vector<Variable>& parameters);
+    void addOperator(TypeSymbol* returnType,
+                     const std::wstring& operatorType,
+                     const std::vector<Variable>& parameters);
+};
 
 struct NamespaceSymbol
     {
         std::wstring name;
+        NamespaceSymbol* parent = nullptr;
         std::vector<NamespaceSymbol> namespaces;
         std::vector<TypeSymbol*> structures;
         std::vector<std::wstring> attributeBlocks;
@@ -68,6 +90,14 @@ struct NamespaceSymbol
         std::unordered_map<std::wstring, std::vector<FunctionSymbol>> functionSignatures;
         std::vector<FunctionSymbol> functions;
         void print(std::wostream& os, size_t indent = 0) const;
+
+        TypeSymbol* addType(const std::wstring& name,
+                            TypeSymbol::Role role = TypeSymbol::Role::Structure);
+        TypeSymbol* typeSymbol(const std::wstring& typeName) const;
+
+        void addFunction(TypeSymbol* returnType,
+                         const std::wstring& name,
+                         const std::vector<Variable>& parameters);
     };
 
     struct PipelineStage
@@ -77,7 +107,7 @@ struct NamespaceSymbol
         void print(std::wostream& os, size_t indent = 0) const;
     };
 
-    struct ShaderData
+struct ShaderData
     {
         NamespaceSymbol globalNamespace;
         std::vector<PipelineStage> pipelineStages;
@@ -85,5 +115,8 @@ struct NamespaceSymbol
     };
 
     std::wostream& operator<<(std::wostream& os, const ShaderData& data);
+
+    std::wstring composeSignature(const std::wstring& name,
+                                  const std::vector<Variable>& parameters);
 }
 
