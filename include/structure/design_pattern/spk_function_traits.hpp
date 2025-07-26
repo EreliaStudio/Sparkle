24db48d0ff3ec3e39a88f0e49ab66159ef366411
Hyarius <Hyarius@erelia-studio.com>
2025-07-26 22:12:34 +0200

    Updating the event dispatcher

---
+++ include/structure/design_pattern/spk_function_traits.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <tuple>
+#include <type_traits>
+
+namespace spk
+{
+	template <typename T>
+	struct FunctionTraits : FunctionTraits<decltype(&T::operator())>
+	{
+
+	};
+
+	template <typename C, typename R, typename... Args>
+	struct FunctionTraits<R (C::*)(Args...) const>
+	{
+		using ArgsTuple = std::tuple<Args...>;
+	};
+
+	template <typename C, typename R, typename... Args>
+	struct FunctionTraits<R (C::*)(Args...)>
+	{
+		using ArgsTuple = std::tuple<Args...>;
+	};
+
+	template <typename R, typename... Args>
+	struct FunctionTraits<R (*)(Args...)>
+	{
+		using ArgsTuple = std::tuple<Args...>;
+	};
+
+	template <typename R, typename... Args>
+	struct FunctionTraits<R(Args...)>
+	{
+		using ArgsTuple = std::tuple<Args...>;
+	};
+
+	template <typename C, typename R, typename... Args>
+	struct FunctionTraits<R (C::*)(Args...) const noexcept> : FunctionTraits<R (C::*)(Args...) const>
+	{
+	};
+
+	template <typename C, typename R, typename... Args>
+	struct FunctionTraits<R (C::*)(Args...) noexcept> : FunctionTraits<R (C::*)(Args...)>
+	{
+	};
+}
