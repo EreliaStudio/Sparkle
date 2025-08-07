e426019b088d5ed04abb18a3fa591ac66643b87c
Erelia Studio <hyarius@erelia-studio.com>
2025-08-07 16:35:34 +0200

    Rename tilemap system and add voxel renderer (#64)

---
+++ include/structure/engine/spk_voxel_vertex.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "structure/math/spk_vector3.hpp"
+#include "structure/math/spk_vector2.hpp"
+
+namespace spk
+{
+        struct VoxelVertex
+        {
+                spk::Vector3 position = {0, 0, 0};
+                spk::Vector2 uv = {0, 0};
+
+                bool operator==(const VoxelVertex &p_other) const
+                {
+                        return (position == p_other.position && uv == p_other.uv);
+                }
+
+                bool operator<(const VoxelVertex &p_other) const
+                {
+                        if ((position != p_other.position) == true)
+                        {
+                                return (position < p_other.position);
+                        }
+                        return (uv < p_other.uv);
+                }
+        };
+}
+
+namespace std
+{
+        template <>
+        struct hash<spk::VoxelVertex>
+        {
+                std::size_t operator()(const spk::VoxelVertex &p_vertex) const
+                {
+                        std::size_t h1 = std::hash<spk::Vector3>{}(p_vertex.position);
+                        std::size_t h2 = std::hash<spk::Vector2>{}(p_vertex.uv);
+                        std::size_t seed = h1;
+                        seed ^= h2 + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+                        return (seed);
+                }
+        };
+}
