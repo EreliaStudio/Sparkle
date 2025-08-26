ce87f32e243ce840e675702ebe4b72c6b243e9c9
Erelia Studio <hyarius@erelia-studio.com>
2025-08-26 21:37:52 +0200

    merge collision faces and restore occlusion

--- src/structure/engine/spk_collision_mesh.cpp
+++ src/structure/engine/spk_collision_mesh.cpp
@@ -14,21 +14,49 @@ namespace spk
 
 	CollisionMesh CollisionMesh::fromObjMesh(const spk::SafePointer<spk::ObjMesh> &p_mesh)
 	{
-		spk::CollisionMesh result;
+		std::vector<spk::Polygon> polys;
 		for (const auto &shapeVariant : p_mesh->shapes())
 		{
-			spk::CollisionMesh::Unit unit;
+			spk::Polygon poly;
 			if (std::holds_alternative<spk::ObjMesh::Quad>(shapeVariant) == true)
 			{
 				const auto &q = std::get<spk::ObjMesh::Quad>(shapeVariant);
-				unit = spk::Polygon::makeSquare(q.a.position, q.b.position, q.c.position, q.d.position);
+				poly = spk::Polygon::makeSquare(q.a.position, q.b.position, q.c.position, q.d.position);
 			}
 			else
 			{
 				const auto &t = std::get<spk::ObjMesh::Triangle>(shapeVariant);
-				unit = spk::Polygon::makeTri(t.a.position, t.b.position, t.c.position);
+				poly = spk::Polygon::makeTri(t.a.position, t.b.position, t.c.position);
 			}
-			result.addUnit(unit);
+			polys.push_back(poly);
+		}
+
+		bool merged = true;
+		while (merged == true)
+		{
+			merged = false;
+			for (size_t i = 0; i < polys.size(); ++i)
+			{
+				for (size_t j = i + 1; j < polys.size();)
+				{
+					if (polys[i].isCoplanar(polys[j]) == true && (polys[i].isAdjacent(polys[j]) == true || polys[i].isOverlapping(polys[j]) == true))
+					{
+						polys[i] = polys[i].fuze(polys[j]);
+						polys.erase(polys.begin() + j);
+						merged = true;
+					}
+					else
+					{
+						++j;
+					}
+				}
+			}
+		}
+
+		CollisionMesh result;
+		for (const auto &poly : polys)
+		{
+			result.addUnit(poly);
 		}
 		return (result);
 	}
