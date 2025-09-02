babc30885da2497f9b25c55a2323fc87e17b3ef7
Hyarius <Hyarius@erelia-studio.com>
2025-09-02 18:46:19 +0200

    Working on refactoring the debug overlay

--- include/widget/spk_debug_overlay.hpp
+++ include/widget/spk_debug_overlay.hpp
@@ -1,103 +1,46 @@
+
 #pragma once
 
+#include "widget/spk_linear_layout.hpp"
 #include "widget/spk_text_label.hpp"
 
 #include "structure/system/spk_exception.hpp"
 
 namespace spk
 {
-	template <size_t TColumn, size_t TRow>
 	class DebugOverlay : public spk::Widget
 	{
-		static_assert(TColumn > 0, "DebugOverlay: TColumn must be greater than 0.");
-		static_assert(TRow > 0, "DebugOverlay: TRow must be greater than 0.");
-
-	public:
-		static inline const size_t Column = TColumn;
-		static inline const size_t Row = TRow;
-
 	private:
-		size_t _outlineSize = 0;
-		std::unique_ptr<spk::TextLabel> _labels[Column][Row];
-
-		void _onGeometryChange() override
+		struct Row
 		{
-			spk::Font::Size fontSize = spk::Font::Size(geometry().size.y, _outlineSize);
+			spk::HorizontalLayout layout;
+			std::vector<std::unique_ptr<spk::TextLabel>> labels;
+		};
 
-			spk::Vector2UInt textLabelSize = geometry().size / spk::Vector2UInt(Column, Row);
+		spk::VerticalLayout _layout;
+		std::vector<Row> _rows;
 
-			for (size_t i = 0; i < Column; i++)
-			{
-				for (size_t j = 0; j < Row; j++)
-				{
-					spk::Font::Size tmpSize = _labels[i][j]->font()->computeOptimalTextSize(_labels[i][j]->text(), 0, textLabelSize);
+		size_t _outlineSize = 0;
+		spk::SafePointer<spk::Font> _font = nullptr;
+		spk::Color _glyphColor = spk::Color::white;
+		spk::Color _outlineColor = spk::Color::black;
 
-					fontSize.glyph = std::min(fontSize.glyph, tmpSize.glyph - _outlineSize);
-				}
-			}
+		void _compose();
+		void _applyFontsToFit();
+		void _ensureRow(const size_t &p_row);
+		void _ensureCol(const size_t &p_row, const size_t &p_col);
 
-			for (size_t i = 0; i < Column; i++)
-			{
-				for (size_t j = 0; j < Row; j++)
-				{
-					_labels[i][j]->setFontSize(fontSize);
-					_labels[i][j]->setGeometry(textLabelSize * spk::Vector2Int(i, j), textLabelSize);
-				}
-			}
-		}
+		void _onGeometryChange() override;
 
 	public:
-		DebugOverlay(const std::wstring &p_name, spk::SafePointer<spk::Widget> p_parent) :
-			spk::Widget(p_name, p_parent)
-		{
-			for (size_t i = 0; i < Column; i++)
-			{
-				for (size_t j = 0; j < Row; j++)
-				{
-					_labels[i][j] =
-						std::make_unique<spk::TextLabel>(p_name + L" - Label [" + std::to_wstring(i) + L"][" + std::to_wstring(j) + L"]", this);
-					_labels[i][j]->setNineSlice(nullptr);
-					_labels[i][j]->activate();
-				}
-			}
-		}
+		DebugOverlay(const std::wstring &p_name, spk::SafePointer<spk::Widget> p_parent);
 
-		void setText(const size_t &p_col, const size_t &p_row, const std::wstring &p_text)
-		{
-			if (p_col >= Column || p_row >= Row)
-			{
-				throw std::invalid_argument("Can't set debug overlay [" + std::to_string(Column) + " / " + std::to_string(Row) +
-											"] text at coordinate [" + std::to_string(p_col) + " / " + std::to_string(p_row) + "]");
-			}
-			_labels[p_col][p_row]->setText(p_text);
-		}
+		void configureRows(const size_t &p_rows, const size_t &p_defaultColumns);
+		void setRowColumns(const size_t &p_row, const size_t &p_columns);
 
-		void setFont(spk::SafePointer<spk::Font> p_font)
-		{
-			for (size_t i = 0; i < Column; i++)
-			{
-				for (size_t j = 0; j < Row; j++)
-				{
-					_labels[i][j]->setFont(p_font);
-				}
-			}
-		}
-
-		void setFontColor(const spk::Color &p_glyphColor, const spk::Color &p_outlineColor)
-		{
-			for (size_t i = 0; i < Column; i++)
-			{
-				for (size_t j = 0; j < Row; j++)
-				{
-					_labels[i][j]->setFontColor(p_glyphColor, p_outlineColor);
-				}
-			}
-		}
-
-		void setFontOutlineSize(size_t p_outlineSize)
-		{
-			_outlineSize = p_outlineSize;
-			requireGeometryUpdate();
-		}
+		void setText(const size_t &p_row, const size_t &p_col, const std::wstring &p_text);
+		void setFont(spk::SafePointer<spk::Font> p_font);
+		void setFontColor(const spk::Color &p_glyphColor, const spk::Color &p_outlineColor);
+		void setFontOutlineSize(size_t p_outlineSize);
 	};
 }
