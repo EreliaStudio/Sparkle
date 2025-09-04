5e51e4be7e893e87d86009447dd79324b8ac8853
Erelia Studio <hyarius@erelia-studio.com>
2025-09-04 14:24:57 +0200

    Use polygon caches for rigid body collisions

--- include/structure/engine/spk_rigid_body.hpp
+++ include/structure/engine/spk_rigid_body.hpp
@@ -2,7 +2,9 @@
 
 #include "structure/engine/spk_collision_mesh.hpp"
 #include "structure/engine/spk_component.hpp"
+#include "structure/math/spk_bounding_box.hpp"
 #include "structure/math/spk_matrix.hpp"
+#include "structure/math/spk_polygon.hpp"
 #include "structure/math/spk_vector3.hpp"
 #include "structure/spk_safe_pointer.hpp"
 #include <mutex>
@@ -15,10 +17,13 @@ namespace spk
 	private:
 		ContractProvider::Contract _onOwnerOnTransformEditionContract;
 		spk::SafePointer<const spk::CollisionMesh> _collisionMesh;
+		spk::BoundingBox _boundingBox;
+		std::vector<spk::Polygon> _polygons;
 		static inline std::vector<spk::SafePointer<RigidBody>> _rigidBodies;
 		static inline std::mutex _rigidBodiesMutex;
 
 		std::vector<spk::SafePointer<const RigidBody>> _executeCollisionTest();
+		void _updateCollisionCache();
 
 	public:
 		RigidBody(const std::wstring &p_name);
@@ -31,7 +36,7 @@ namespace spk
 		void setCollisionMesh(const spk::SafePointer<const spk::CollisionMesh> &p_collisionMesh);
 		const spk::SafePointer<const spk::CollisionMesh> &collisionMesh() const;
 
-		static const std::vector<spk::SafePointer<RigidBody>>& getRigidBodies();
+		static const std::vector<spk::SafePointer<RigidBody>> &getRigidBodies();
 
 		bool intersect(const spk::SafePointer<RigidBody> p_other) const;
 	};
