083f8f6ca766016443b08aa7bddc3646b170c3b5
Hyarius <Hyarius@erelia-studio.com>
2025-09-08 11:08:05 +0200

    Fix up some stuff, but the shapes aren't rendering yet

--- src/structure/graphics/opengl/spk_shader_storage_buffer_object.cpp
+++ src/structure/graphics/opengl/spk_shader_storage_buffer_object.cpp
@@ -43,7 +43,6 @@ namespace spk::OpenGL
 
 	void ShaderStorageBufferObject::DynamicArray::_redoArray()
 	{
-		spk::cout << "Element size : " << _elements.size() << std::endl;
 		for (size_t i = 0; i < _elements.size(); ++i)
 		{
 			auto &element = _elements[i];
@@ -101,13 +100,11 @@ namespace spk::OpenGL
 
 	ShaderStorageBufferObject::DynamicArray::Element &ShaderStorageBufferObject::DynamicArray::operator[](size_t p_index)
 	{
-		DEBUG_LINE();
 		return _elements[p_index];
 	}
 
 	const ShaderStorageBufferObject::DynamicArray::Element &ShaderStorageBufferObject::DynamicArray::operator[](size_t p_index) const
 	{
-		DEBUG_LINE();
 		return _elements[p_index];
 	}
 
@@ -130,9 +127,7 @@ namespace spk::OpenGL
 		_dynamicArray(L"DynamicArray", &dataBuffer(), 0, 0, 0)
 	{
 		_onResizeContract = _dynamicArray._resizeContractProvider.subscribe([&](size_t p_size) {
-			DEBUG_LINE();
 			resize(p_size);
-			DEBUG_LINE();
 		});
 		_dynamicArray._buffer = &dataBuffer();
 	}
