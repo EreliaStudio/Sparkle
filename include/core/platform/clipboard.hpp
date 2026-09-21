0c00a0af76e118a0488e9fabe6200d41456a4c88
Hyarius <hyarius@erelia-studio.com>
2026-09-21 17:18:27 +0200

    Introduce clipboard backend abstraction

--- include/core/platform/clipboard.hpp
+++ include/core/platform/clipboard.hpp
@@ -6,6 +6,18 @@
 
 namespace spk::Clipboard
 {
+	class Backend
+	{
+	public:
+		virtual ~Backend() = default;
+
+		[[nodiscard]] virtual bool hasText() const = 0;
+		[[nodiscard]] virtual std::optional<Font::Text> readText() const = 0;
+		virtual bool writeText(const Font::Text &text) = 0;
+	};
+
+	[[nodiscard]] Backend &systemBackend() noexcept;
+
 	[[nodiscard]] bool hasText();
 	[[nodiscard]] std::optional<Font::Text> readText();
 	bool writeText(const Font::Text &text);
