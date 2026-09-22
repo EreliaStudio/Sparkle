696c895438542f71fef9e6677e23fa4a7894a11e
Hyarius <hyarius@erelia-studio.com>
2026-09-22 14:42:56 +0200

    Replace Asio client with internal socket backend

--- src/network/client.cpp
+++ src/network/client.cpp
@@ -2,15 +2,11 @@
 
 #include "exception.hpp"
 #include "network/internal/frame.hpp"
-
-#include <asio.hpp>
+#include "network/internal/socket.hpp"
 
 #include <array>
 #include <atomic>
-#include <deque>
-#include <future>
-#include <optional>
-#include <string>
+#include <mutex>
 #include <thread>
 #include <utility>
 #include <vector>
@@ -20,148 +16,59 @@ namespace spk
 	class Client::Impl
 	{
 	private:
-		using TCP = asio::ip::tcp;
-		using WorkGuard = asio::executor_work_guard<asio::io_context::executor_type>;
-
 		Client &_owner;
-		asio::io_context _context;
-		std::optional<WorkGuard> _work;
-		std::unique_ptr<TCP::socket> _socket;
+		NetworkInternal::Socket _socket;
 		std::jthread _worker;
 		std::atomic_bool _connected = false;
-		std::array<std::byte, NetworkInternal::FrameHeaderSize> _header{};
-		std::vector<std::byte> _payload;
-		std::deque<std::shared_ptr<std::vector<std::byte>>> _writes;
+		std::mutex _sendMutex;
 
-		[[nodiscard]] bool _onWorkerThread() const noexcept
-		{
-			return _worker.joinable() && _worker.get_id() == std::this_thread::get_id();
-		}
-
-		void _readHeader()
-		{
-			asio::async_read(*_socket, asio::buffer(_header), [this](const std::error_code &error, std::size_t) {
-				if (error)
-				{
-					_shutdown();
-					return;
-				}
-				_handleHeader();
-			});
-		}
-
-		void _handleHeader()
+		void _run()
 		{
 			try
 			{
-				const auto header = NetworkInternal::decode(_header);
-				_readPayload(header);
+				while (_connected)
+				{
+					std::array<std::byte, NetworkInternal::FrameHeaderSize> bytes{};
+					if (!_socket.receiveAll(bytes))
+					{
+						break;
+					}
+
+					const auto header = NetworkInternal::decode(bytes);
+					Message::Storage payload(header.payloadSize);
+					if (!payload.empty() && !_socket.receiveAll(payload))
+					{
+						break;
+					}
+					_owner._publish(Message(header.type, std::move(payload)));
+				}
 			}
 			catch (...)
 			{
-				_shutdown();
 			}
+			_remoteDisconnect();
 		}
 
-		void _readPayload(NetworkInternal::FrameHeader header)
-		{
-			_payload.resize(header.payloadSize);
-			if (_payload.empty())
-			{
-				_owner._publish(Message(header.type));
-				_readHeader();
-				return;
-			}
-			asio::async_read(*_socket, asio::buffer(_payload), [this, header](const std::error_code &error, std::size_t) {
-				if (error)
-				{
-					_shutdown();
-					return;
-				}
-				_owner._publish(Message(header.type, std::move(_payload)));
-				_readHeader();
-			});
-		}
-
-		void _queueWrite(std::shared_ptr<std::vector<std::byte>> frame)
-		{
-			const bool idle = _writes.empty();
-			_writes.push_back(std::move(frame));
-			if (idle)
-			{
-				_writeNext();
-			}
-		}
-
-		void _writeNext()
-		{
-			if (_writes.empty() || !_connected)
-			{
-				return;
-			}
-			asio::async_write(*_socket, asio::buffer(*_writes.front()), [this](const std::error_code &error, std::size_t) {
-				if (error)
-				{
-					_shutdown();
-					return;
-				}
-				_writes.pop_front();
-				_writeNext();
-			});
-		}
-
-		void _closeSocket() noexcept
-		{
-			if (_socket == nullptr)
-			{
-				return;
-			}
-			std::error_code ignored;
-			_socket->cancel(ignored);
-			_socket->shutdown(TCP::socket::shutdown_both, ignored);
-			_socket->close(ignored);
-		}
-
-		void _shutdown()
+		void _remoteDisconnect() noexcept
 		{
 			if (!_connected.exchange(false))
 			{
 				return;
 			}
-			_closeSocket();
-			_writes.clear();
-			_work.reset();
-			_owner._notifyDisconnection();
-		}
 
-		template <typename TFunction>
-		void _invoke(TFunction function)
-		{
-			if (_onWorkerThread())
+			_socket.close();
+			try
 			{
-				function();
-				return;
+				_owner._notifyDisconnection();
 			}
-
-			auto completion = std::make_shared<std::promise<void>>();
-			auto future = completion->get_future();
-			asio::post(_context, [function = std::move(function), completion]() mutable {
-				try
-				{
-					function();
-					completion->set_value();
-				}
-				catch (...)
-				{
-					completion->set_exception(std::current_exception());
-				}
-			});
-			future.get();
+			catch (...)
+			{
+			}
 		}
 
 		void _joinWorker()
 		{
-			if (_worker.joinable() && !_onWorkerThread())
+			if (_worker.joinable() && _worker.get_id() != std::this_thread::get_id())
 			{
 				_worker.join();
 			}
@@ -192,60 +99,54 @@ namespace spk
 		void connect(std::string_view address, std::uint16_t port)
 		{
 			disconnect();
-			_context.restart();
-			_work.emplace(_context.get_executor());
-			_socket = std::make_unique<TCP::socket>(_context);
 
 			try
 			{
-				TCP::resolver resolver(_context);
-				const auto endpoints = resolver.resolve(std::string(address), std::to_string(port));
-				asio::connect(*_socket, endpoints);
+				_socket = NetworkInternal::Socket::connectTCP(address, port);
 			}
 			catch (...)
 			{
-				_work.reset();
-				_socket.reset();
 				throw Exception("Unable to connect the network client.", std::current_exception());
 			}
 
 			_connected = true;
-			_readHeader();
 			_worker = std::jthread([this] {
-				_context.run();
+				_run();
 			});
-			_owner._notifyConnection();
+
+			try
+			{
+				_owner._notifyConnection();
+			}
+			catch (...)
+			{
+				disconnect();
+				throw;
+			}
 		}
 
 		void disconnect()
 		{
-			if (_connected)
-			{
-				_invoke([this] {
-					_shutdown();
-				});
-			}
+			const bool notify = _connected.exchange(false);
+			_socket.close();
 			_joinWorker();
-			_work.reset();
-			_context.stop();
-			_socket.reset();
-			_writes.clear();
+
+			if (notify)
+			{
+				_owner._notifyDisconnection();
+			}
 		}
 
 		void send(const Message &message)
 		{
+			const auto frame = NetworkInternal::encode(message);
+			const std::scoped_lock lock(_sendMutex);
+
 			if (!_connected)
 			{
 				throw Exception("Unable to send through a disconnected network client.");
 			}
-
-			auto frame = NetworkInternal::encode(message);
-			asio::post(_context, [this, frame = std::move(frame)] {
-				if (_connected)
-				{
-					_queueWrite(std::move(frame));
-				}
-			});
+			_socket.sendAll(*frame);
 		}
 	};
 
