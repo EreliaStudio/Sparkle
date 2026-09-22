c6453918b6b8d61b283d7be384cc3f86a1a7749a
Hyarius <hyarius@erelia-studio.com>
2026-09-22 14:07:58 +0200

    Add portable networking API: include/network/network.hpp

---
+++ include/network/network.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "network/client.hpp"
+#include "network/local_node.hpp"
+#include "network/message.hpp"
+#include "network/node.hpp"
+#include "network/node_router.hpp"
+#include "network/server.hpp"
+#include "network/types.hpp"
