2d73f0e0ac953c845ed8b0a407fada1f1c6c3ad8
Hyarius <hyarius@erelia-studio.com>
2026-09-22 15:27:40 +0200

    Allow RemoteNode dispatch on a dedicated thread

--- include/network/remote_node.hpp
+++ include/network/remote_node.hpp
@@ -11,17 +11,24 @@ namespace spk
 {
 	class RemoteNode final : public Node
 	{
+	public:
+		using IncomingQueue = ThreadSafeFIFO<ReceivedMessage>;
+
 	private:
 		Client _client;
+		IncomingQueue _incoming;
 		OutgoingQueue _outgoing;
+		std::vector<ReceivedMessage> _requests;
 		std::vector<Message> _responses;
 
+		void _forwardRequests();
 		void _collectResponses();
 
 	public:
 		[[nodiscard]] bool isConnected() const noexcept;
 		void connect(std::string_view address, std::uint16_t port);
 		void disconnect();
+		void dispatch();
 
 		void receive(ReceivedMessage message) override;
 		[[nodiscard]] OutgoingQueue &outgoing() override;
